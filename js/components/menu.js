5627e61e4afaf433faaaeb5d2b0fb922d3a7e66e
David <davie668@gmail.com>
2021-08-21 15:41:16 +0100

    Refactored menu

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import {Component, createElement} from 'react';
 import {MenuItem} from "./menuItem.js";
 
-export class Menu extends React.Component {
-    render() {
-        const {menuItems, addToBasket} = this.props
+export class Menu extends Component {
+    state = {
+        menuItems: []
+    }
 
-        return React.createElement('div', {},
+    get #menuItems() {
+        return this.state.menuItems
+    }
+
+    async componentDidMount() {
+        const menuItems = await this.getMenuItems()
+        this.setState({
+            menuItems: menuItems
+        })
+    }
+
+    getMenuItems() {
+        return new Promise((resolve) => {
+            const menuItems = [
+                {name: "Soup"},
+                {name: "Sandwich"},
+                {name: "Cola"},
+            ]
+            resolve(menuItems)
+        })
+    }
+
+    get #addToBasket() {
+        const {addToBasket} = this.props
+        return addToBasket
+    }
+
+    get #menuItemElements() {
+        return this.#menuItems.map(item => {
+            return createElement(MenuItem, {item: item, addToBasket: this.#addToBasket})
+        })
+    }
+
+    render() {
+        return createElement('div', {},
             [
-                React.createElement("h2", {}, "Menu"),
-                React.createElement("ul", {}, menuItems.map(item => {
-                    return React.createElement(MenuItem, {item: item, addToBasket: addToBasket})
-                }))
+                createElement("h2", {}, "Menu"),
+                createElement("ul", {}, this.#menuItemElements)
             ]
         )
     }
--- js/components/pos.js
+++ js/components/pos.js
@@ -3,19 +3,12 @@ import {Basket} from "./basket.js";
 import {Menu} from "./menu.js";
 
 export class POS extends React.Component {
+    state = {
+        basket: []
+    };
+
     constructor(props) {
         super(props);
-        this.state = {
-            menuItems: [
-                {name: "Soup"},
-                {name: "Sandwich"},
-                {name: "Cola"},
-            ],
-            basket: [
-                {name: "Sample Item"}
-            ]
-        };
-
         this.addToBasket = this.addToBasket.bind(this)
     }
 
@@ -37,7 +30,7 @@ export class POS extends React.Component {
 
         const menu = React.createElement(
             Menu,
-            {menuItems: this.state.menuItems, addToBasket: this.addToBasket}
+            {addToBasket: this.addToBasket}
         )
 
         return React.createElement('div', {}, [
